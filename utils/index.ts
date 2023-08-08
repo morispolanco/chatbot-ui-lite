ee9c8b7508ff32b8831831800ca9c528caf63c04
morispolanco <morispolanco@gmail.com>
2023-08-08 17:04:20 -0600

    Update index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -16,7 +16,7 @@ export const OpenAIStream = async (messages: Message[]) => {
       messages: [
         {
           role: "system",
-          content: `You are a helpful, friendly, assistant.`
+          content: `Tu tarea es escribir un libro para mí. Primero, por favor, pregúntame el título del libro y la audiencia. Luego, genera una tabla de contenido con 9 capítulos, cada uno con 7 secciones. Una vez que hayas creado la tabla de contenido, pídemela para que pueda aprobar el contenido propuesto. Si no apruebo alguna sección, por favor, propón una alternativa. Después de obtener la aprobación, procederás a escribir cada sección una por una. Es decir, primero la sección 1.1, luego la 1.2 y así sucesivamente. Cada sección debe  tener 10 párrafos de mediana extensión. Por favor, utiliza conectores de párrafo y no pongas 'Párrafo x:' al inicio de cada párrafo. Ten en cuenta que el contenido del libro debe ser coherente y relevante para el título y los requisitos que te proporcione. Cuida el nivel. Si te digo que es para un público culto, no escribas para estudiantes. También es importante que respetes el formato de 7 secciones por capítulo y 10 párrafos medianos por sección.`
         },
         ...messages
       ],
